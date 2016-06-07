85b0d74c15df671d9f0d2b36038a23e70331dec5
paulkenrick <pkenrick@gmail.com>
2016-06-07 15:47:09 +0100

    Comparing post and get paths

--- app.rb
+++ app.rb
@@ -18,7 +18,29 @@ end
 get '/named-cat' do
 	p params				# prints '{"name"=>"paul"}' to console
 	@name = params[:name]
-	@age = params[:age]
+  age = params[:age]
 	erb(:index)
+end
+
+# add '?name=paul' to end of URL 
+post '/named-cat' do
+  p params        # prints '{"name"=>"paul"}' to console
+  @name = params[:name]
+  @age = params[:age]
+  erb(:index)
+end
+
+get '/cat-form' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  erb(:cat_form)
+end
+
+post '/cat-form' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  erb(:cat_form)
 end
 
